06e4728d4ff5a3764b971459ec475026f1734f7c
GuYue <2472152403@qq.com>
2023-07-26 15:31:27 +0800

    MyBatis-搭建MyBatis框架（优化添加用户功能

--- mybatis_helloworld/src/test/java/com/gy/mybatis/test/MyBatisTest.java
+++ mybatis_helloworld/src/test/java/com/gy/mybatis/test/MyBatisTest.java
@@ -24,15 +24,22 @@ public class MyBatisTest {
         SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
         // 获取SqlSessionFactory对象
         SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(is);
-        // 获取SQL的会话对象SqlSession，是MyBatis提供的操作数据库的对象
-        SqlSession sqlSession = sqlSessionFactory.openSession();
+        // 获取SQL的会话对象SqlSession(不会自动提交事务)，是MyBatis提供的操作数据库的对象
+        // SqlSession sqlSession = sqlSessionFactory.openSession();
+        // 获取SQL的会话对象SqlSession(会自动提交事务)，是MyBatis提供的操作数据库的对象
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+
         // 获取UserMapper的代理实现类对象
         UserMapper mapper = sqlSession.getMapper(UserMapper.class);
         // 调用mapper接口中的方法，实现添加用户信息的功能
         int result = mapper.insertUser();
+
+        // 通过sql一级的唯一标识找到sql并执行，唯一标识是namespace.sqlId
+        // int result = sqlSession.insert("com.gy.mybatis.mapper.UserMapper.insertUser");
+
         System.out.println("结果：" + result);
         // 提交事务
-        sqlSession.commit();
+        // sqlSession.commit();
         // 关闭SqlSession
         sqlSession.close();
     }
