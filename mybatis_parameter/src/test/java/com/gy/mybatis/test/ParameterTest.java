ba4b3e990da885e6f6cb2a18892ac0aa13e8cdf5
GuYue <2472152403@qq.com>
2023-07-27 15:22:31 +0800

    MyBatis-MyBatis获取参数值的情况

---
+++ mybatis_parameter/src/test/java/com/gy/mybatis/test/ParameterTest.java
@@ -0,0 +1,62 @@
+package com.gy.mybatis.test;
+
+import com.gy.mybatis.mapper.UserMapper;
+import com.gy.mybatis.pojo.User;
+import com.gy.mybatis.utils.SqlSessionUtil;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Guyue
+ * @date 2023/7/27 11:05
+ */
+public class ParameterTest {
+
+    @Test
+    public void testGetUserByUsername() {
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User admin = mapper.getUserByUsername("admin");
+        System.out.println(admin);
+    }
+
+    @Test
+    public void testCheckLogin() {
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User user = mapper.checkLogin("admin", "123456");
+        System.out.println(user);
+    }
+
+    @Test
+    public void testCheckLoginByMap() {
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        Map<String, Object> map = new HashMap<>();
+        map.put("username", "admin");
+        map.put("password", "123456");
+        User user = mapper.checkLoginByMap(map);
+        System.out.println(user);
+    }
+
+    @Test
+    public void testInsertUser() {
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User user = new User(null, "root", "123456", 33, "女", "12345@qq.com");
+        mapper.insertUser(user);
+    }
+
+    @Test
+    public void testCheckLoginByParam() {
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User user = mapper.checkLoginByParam("admin", "123456");
+        System.out.println(user);
+    }
+
+
+}
