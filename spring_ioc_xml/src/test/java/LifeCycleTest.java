8748c1001868d75651da7fc3a11db55cc9643603
gy <2472152403@qq.com>
2023-08-04 16:29:44 +0800

    spring-bean的作用域对生命周期的影响和后置处理器

--- spring_ioc_xml/src/test/java/LifeCycleTest.java
+++ spring_ioc_xml/src/test/java/LifeCycleTest.java
@@ -12,8 +12,18 @@ public class LifeCycleTest {
     /**
      * 1、实例化
      * 2、依赖注入
-     * 3、初始化，需要通过bean的init-method属性指定初始化的方法
-     * 4、IOC容器关闭时销毁，需要通过bean的destroy-method属性指定销毁的方法
+     * 3、后置处理器的 postProcessBeforeInitialization
+     * 4、初始化，需要通过bean的init-method属性指定初始化的方法
+     * 5、后置处理器的 postProcessAfterInitialization
+     * 6、IOC容器关闭时销毁，需要通过bean的destroy-method属性指定销毁的方法
+     *
+     * bean的后置处理器会在生命周期的初始化前后添加额外的操作
+     * 需要实现BeanPostProcessor接口且配置到IOC容器中，
+     * 需要注意的是，bean后置处理器不是单独针对某一个bean生效，而是针对IOC容器中所有bean都会执行
+     *
+     * 注意：
+     * 若bean的作用域为单例时，生命周期的前三个步骤会在获取IOC容器时执行
+     * 若bean的作用域为多例时，生命周期的前三个步骤会在获取bean时执行
      */
     @Test
     public void test() {
