43a4e12a6e62db1c8ac3731f5cc03ac6b70134bf
gy <2472152403@qq.com>
2023-08-04 12:12:39 +0800

    spring-bean的作用域

---
+++ spring_ioc_xml/src/test/java/ScopeTest.java
@@ -0,0 +1,19 @@
+import com.gy.spring.pojo.Student;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author Guyue
+ * @date 2023/8/4 11:55
+ */
+public class ScopeTest {
+
+    @Test
+    public void testScope() {
+        ApplicationContext ioc = new ClassPathXmlApplicationContext("spring-scope.xml");
+        Student student1 = ioc.getBean(Student.class);
+        Student student2 = ioc.getBean(Student.class);
+        System.out.println(student1 == student2);
+    }
+}
