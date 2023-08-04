bc13ccdb0906274549436452e190308c4038c09b
gy <2472152403@qq.com>
2023-08-04 11:28:44 +0800

    spring-Spring管理数据库源和引入外部属性文件

---
+++ spring_ioc_xml/src/test/java/DataSourceTest.java
@@ -0,0 +1,21 @@
+import com.alibaba.druid.pool.DruidDataSource;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.sql.SQLException;
+
+/**
+ * @author Guyue
+ * @date 2023/8/4 11:15
+ */
+public class DataSourceTest {
+
+    @Test
+    public void testDataSource() throws SQLException {
+        ApplicationContext ioc = new ClassPathXmlApplicationContext("spring-datasource.xml");
+        DruidDataSource dataSource = ioc.getBean(DruidDataSource.class);
+        System.out.println(dataSource.getConnection());
+    }
+
+}
