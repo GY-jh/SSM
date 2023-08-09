9170a9b9f3f020d9098e9503b9292b472dc4628e
GuYue <2472152403@qq.com>
2023-08-09 16:56:09 +0800

    spring-基于注解的AOP（准备工作

---
+++ spring_aop/src/main/java/com/gy/spring/aop/annotation/Calculator.java
@@ -0,0 +1,15 @@
+package com.gy.spring.aop.annotation;
+
+/**
+ * @author Guyue
+ * @date 2023/8/9 10:55
+ */
+public interface Calculator {
+    int add(int i, int j);
+
+    int sub(int i, int j);
+
+    int mul(int i, int j);
+
+    int div(int i, int j);
+}
