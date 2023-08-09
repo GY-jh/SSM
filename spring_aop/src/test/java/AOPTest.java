1b54a83ffd6d6d5c2f0ad964765817c146f2f059
GuYue <2472152403@qq.com>
2023-08-09 17:16:42 +0800

    spring-基于注解的AOP（前置通知

---
+++ spring_aop/src/test/java/AOPTest.java
@@ -0,0 +1,19 @@
+import com.gy.spring.aop.annotation.Calculator;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author Guyue
+ * @date 2023/8/9 17:07
+ */
+public class AOPTest {
+
+    @Test
+    public void testAopByAnnotation() {
+        ApplicationContext ioc = new ClassPathXmlApplicationContext("aop-annotation.xml");
+        Calculator calculator = ioc.getBean(Calculator.class);
+        calculator.add(2, 1);
+    }
+
+}
