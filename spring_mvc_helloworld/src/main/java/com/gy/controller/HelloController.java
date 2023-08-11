e63e4b93200d8ac261905b69c52bd6c4eb06978b
GuYue <2472152403@qq.com>
2023-08-11 10:31:28 +0800

    SpringMVC-入门案例（创建SpringMVC的配置文件并配置

---
+++ spring_mvc_helloworld/src/main/java/com/gy/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.gy.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: GuYue
+ * @Date: 2023/8/11 10:16
+ * @Description:
+ */
+@Controller
+public class HelloController {
+}
