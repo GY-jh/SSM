cdf629617dc533a7c9c3152aa4249f591f48e413
GuYue <2472152403@qq.com>
2023-08-11 18:07:14 +0800

    SpringMVC-SpringMVC支持路径中的占位符

--- spring_mvc_demo/src/main/java/com/gy/controller/TestRequestMappingController.java
+++ spring_mvc_demo/src/main/java/com/gy/controller/TestRequestMappingController.java
@@ -1,6 +1,7 @@
 package com.gy.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -47,12 +48,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * "header!=value"：要求请求映射所匹配的请求必须携带header请求头信息且header!=value
  * 若浏览器所发送的请求的请求路径和@RequestMapping注解value属性匹配，但是请求头信息不匹配
  * 此时页面报错：404
- *
+ * <p>
  * 6、SpringMVC支持ant风格的路径
  * 在@RequestMapping注解的value属性值中设置一些特殊制服
  * ?：表示任意的单个字符（不包括?）
  * *：表示任意个数的任意字符（不包括?和/）
  * **：表示任意层数的任意目录，注意使用方式只能**写在双斜线中，前后不能有任何其他字符
+ *
+ * 7、@RequestMapping注解使用路径中的占位符
+ * 传统：/deleteUser?id=1
+ * rest：/user/delete/1
+ * 需要在@RequestMapping注解的value属性中设置的路径中，使用{XXX}的方式表示路径中的数据
+ * 在通过@PathVariable注解，将占位符所标识的值和控制器方法的形参进行绑定
  */
 @Controller
 // @RequestMapping("/test")
@@ -71,6 +78,12 @@ public class TestRequestMappingController {
 
     @RequestMapping("/**/test/ant")
     public String testAnt() {
+        return "success";
+    }
+
+    @RequestMapping("/test/rest/{username}/{id}")
+    public String testRest(@PathVariable("id") Integer id, @PathVariable("username") String username) {
+        System.out.println("id:" + id + "，username:" + username);
         return "success";
     }
 
