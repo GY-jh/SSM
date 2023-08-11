9ce735599a6a75e1d2c1aa674a188e9660d111f9
GuYue <2472152403@qq.com>
2023-08-11 17:10:11 +0800

    SpringMVC-@RequestMapping注解的method属性

--- spring_mvc_demo/src/main/java/com/gy/controller/TestRequestMappingController.java
+++ spring_mvc_demo/src/main/java/com/gy/controller/TestRequestMappingController.java
@@ -2,6 +2,7 @@ package com.gy.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created with IntelliJ IDEA.
@@ -16,13 +17,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * 作用：通过请求的请求路径匹配请求
  * value属性是数组类型，即当前浏览器所发送请求的请求路径匹配value属性中的任何一个值
  * 则当前请求就会被注解所标识的方法进行处理
+ * 3、@RequestMapping注解的method属性
+ * 作用：通过请求的请求方式匹配请求
+ * method属性是RequestMethod类型是数组，即当前浏览器所发送请求的请求方式匹配method属性中任何一种请求方式
+ *则当前请求就会被注解所标识的方法进行处理
+ * 若浏览器所方式的请求的请求路径和@RequestMapping注解value属性匹配，但是请求方式不匹配
+ * 此时页面报错：405 – Request method 'XXX' not supported
  */
 @Controller
 // @RequestMapping("/test")
 public class TestRequestMappingController {
 
     // 此时控制器方法所匹配的请求的请求路径为/test/hello
-    @RequestMapping({"/hello", "/abc"})
+    @RequestMapping(
+            value = {"/hello", "/abc"},
+            method = {RequestMethod.POST, RequestMethod.GET})
     public String hello() {
         return "success";
     }
