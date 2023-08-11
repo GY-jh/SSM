b78b1d1654c5f0d1d424ee028d5de2ae9e8a4b85
GuYue <2472152403@qq.com>
2023-08-11 16:49:53 +0800

    SpringMVC-@RequestMapping注解的value属性

--- spring_mvc_demo/src/main/java/com/gy/controller/TestRequestMappingController.java
+++ spring_mvc_demo/src/main/java/com/gy/controller/TestRequestMappingController.java
@@ -8,16 +8,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
  *
  * @Author: GuYue
  * @Date: 2023/8/11 16:14
- * @Description: 1、@RequestMapping注解标识的位置
+ * @Description :
+ * 1、@RequestMapping注解标识的位置
  * @RequestMapping标识一个类：设置映射请求的请求路径的初始信息
  * @RequestMapping标识一个方法：设置映射请求请求路径的具体信息 .
+ * 2、@RequestMapping注解value属性
+ * 作用：通过请求的请求路径匹配请求
+ * value属性是数组类型，即当前浏览器所发送请求的请求路径匹配value属性中的任何一个值
+ * 则当前请求就会被注解所标识的方法进行处理
  */
 @Controller
-@RequestMapping("/test")
+// @RequestMapping("/test")
 public class TestRequestMappingController {
 
     // 此时控制器方法所匹配的请求的请求路径为/test/hello
-    @RequestMapping("/hello")
+    @RequestMapping({"/hello", "/abc"})
     public String hello() {
         return "success";
     }
