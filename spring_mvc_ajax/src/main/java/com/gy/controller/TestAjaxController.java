b42e6a67cf8c788d0910848371a1121f8f17c606
GuYue <2472152403@qq.com>
2023-08-22 21:26:36 +0800

    SpringMVC-使用@RequestBody注解处理JSON格式的请求参数

--- spring_mvc_ajax/src/main/java/com/gy/controller/TestAjaxController.java
+++ spring_mvc_ajax/src/main/java/com/gy/controller/TestAjaxController.java
@@ -1,10 +1,13 @@
 package com.gy.controller;
 
+import com.gy.pojo.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
@@ -12,14 +15,32 @@ import java.io.IOException;
  * @Author: GuYue
  * @Date: 2023/8/22 20:10
  * @Description :
+ * 1、@RequestBody：将请求体中的内容和控制器方法的形参进行绑定
+ * 2、使用@RequestBody注解将JSON格式的请求参数转换为Java对象
+ * a>导入Jackson的依赖
+ * b>在SpringMVC的配置文件中设置<mvc:default-servlet-handler/>
+ * c>在处理请求的控制器方法的形参位置，直接设置JSON格式的请求参数要转换的Java类型的形参，然后使用@RequestBody注解标识即可
  */
 @Controller
 public class TestAjaxController {
 
     @RequestMapping("/test/ajax")
-    public void testAjax(Integer id, HttpServletResponse response) throws IOException {
+    public void testAjax(Integer id, @RequestBody String requestBody, HttpServletResponse response) throws IOException {
+        System.out.println("requestBody:" + requestBody);
         System.out.println("id:" + id);
         response.getWriter().write("hello,axios");
+    }
+
+
+    @RequestMapping("/test/RequestBody/json")
+    public void testRequestBody(@RequestBody Map<String, Object> map, HttpServletResponse response) throws IOException {
+        System.out.println(map);
+        response.getWriter().write("hello,RequestBody");
+    }
+
+    public void testRequestBody(@RequestBody User user, HttpServletResponse response) throws IOException {
+        System.out.println(user);
+        response.getWriter().write("hello,RequestBody");
     }
 
 }
