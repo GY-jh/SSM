51aa844cf5017201e8907ebb66a3b46ceecc68f5
GuYue <2472152403@qq.com>
2023-08-26 15:23:42 +0800

    SSM-配置Spring

---
+++ ssm/src/main/java/com/gy/ssm/service/EmployeeService.java
@@ -0,0 +1,11 @@
+package com.gy.ssm.service;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: GuYue
+ * @Date: 2023/8/26 15:12
+ * @Description:
+ */
+public interface EmployeeService {
+}
---
+++ ssm/src/main/java/com/gy/ssm/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,13 @@
+package com.gy.ssm.service.impl;
+
+import com.gy.ssm.service.EmployeeService;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: GuYue
+ * @Date: 2023/8/26 15:12
+ * @Description:
+ */
+public class EmployeeServiceImpl implements EmployeeService {
+}
