6049be910ed6bc5e9a5eb668045b1c1bf4d6244e
GuYue <2472152403@qq.com>
2023-08-26 15:11:03 +0800

    SSM-配置SpringMVC

---
+++ ssm/src/main/java/com/gy/ssm/controller/EmployeeController.java
@@ -0,0 +1,14 @@
+package com.gy.ssm.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: GuYue
+ * @Date: 2023/8/26 14:52
+ * @Description:
+ */
+@Controller
+public class EmployeeController {
+}
